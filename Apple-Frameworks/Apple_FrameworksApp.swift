84dfb17541ebf2b48a2dc2454a04b91d2c246a8a
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-02-10 17:40:14 +0100

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Damien Gautier on 10/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
