21080f9ba358af9ffcb003afb9d033a1395f23cc
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-02-13 16:23:57 +0100

    [CHG] changing from NavigationView to NavigationStack

--- Apple-Frameworks/Screens/FrameworkDetailView/FrameworkDetailView.swift
+++ Apple-Frameworks/Screens/FrameworkDetailView/FrameworkDetailView.swift
@@ -9,15 +9,10 @@ import SwiftUI
 
 struct FrameworkDetailView: View {
     var framework: Framework
-    @Binding var isShowingDetailView: Bool
     @State private var isShowingSafariView = false
     
     var body: some View {
         VStack {
-            
-            XDismissButton(isShowingDetailView: $isShowingDetailView)
-            
-            Spacer()
             
             FrameworkTitleView(framework: framework)
             
@@ -46,6 +41,6 @@ struct FrameworkDetailView: View {
 
 struct FrameworkDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        FrameworkDetailView(framework: MockData.sampleFramework, isShowingDetailView: .constant(false))
+        FrameworkDetailView(framework: MockData.sampleFramework)
     }
 }
